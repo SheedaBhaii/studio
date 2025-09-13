f8c62d49b35fe87e40ba803b4cb368b0e54d9f4e
Sheeda Bhaii <bhaiisheeda@gmail.com>
2025-09-13 22:23:26 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,7 +3,7 @@ import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   "projectId": "studio-9915448084-db4ad",
-  "appId": "1:533046792478:web:9844585889f0fc5bd5390f",
+  "appId": "1:533046792478:web:48396561584c76c4d5390f",
   "storageBucket": "studio-9915448084-db4ad.firebasestorage.app",
   "apiKey": "AIzaSyAAaYKgt3KZWoobCVLjPLQi8w6SvFqzsX0",
   "authDomain": "studio-9915448084-db4ad.firebaseapp.com",
